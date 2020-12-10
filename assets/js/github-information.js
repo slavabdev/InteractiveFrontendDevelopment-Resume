28304f3d04828e2814c1c5a472fb7b36ab5a78d9
slavabdev <slav.balykov@gmail.com>
2020-12-10 12:09:11 +0000

    add repos to github page, bu have some bugs in styling

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,3 +1,5 @@
+//THEN ADD THIS 
+// Add a user information to the DOM
 function userInformationHTML(user) { //user is an object that been returned from the GitHub API (to see all the properties of user console.log(user))
     //it will return users public name
     return `
@@ -21,7 +23,31 @@ function userInformationHTML(user) { //user is an object that been returned from
 
  // comment to line 15 -  we want to show a user followers and following and public repos
 
-function fetchGitHubInformation (event){
+function repoInformationHTML(repos) {    //GitHub returns a value of repo in array, so here we will use an array method
+    if (repos.length === 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`
+    }
+let listItemsHTML = repos.map(function(repo){  //map method similar to forEach, but it returns a result in array  
+    return `<li>
+                <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
+});
+
+return `<div class="clearfix repo-list">
+            <p>
+                <strong>Repo List:</strong>
+            </p>    
+                <ul>
+                    ${listItemsHTML.join("\n")}   
+                </ul>
+        </div>`
+}
+
+ //STARTED CODE FROM THIS FUNCTION (below)
+
+ function fetchGitHubInformation(event){
+     $('gh-user-data').html('');
+    $('gh-repo-data').html('');
 
     let username = $("#gh-username").val();
     // first we make a new variable to hold in an id and value  
@@ -39,12 +65,18 @@ function fetchGitHubInformation (event){
     
 //Promise
 $.when(
-    $.getJSON(`https://api.github.com/users/${username}`)
+    $.getJSON(`https://api.github.com/users/${username}`),
+    $.getJSON(`https://api.github.com/users/${username}/repos`)  //now we want to get a response for repos
 ).then(
     // if previous function is done, goes another one
-    function(response) {
-        let userData = response; //we put a response from JSON to userData variable
+    
+    function(firstResponse, secondResponse) {  //By the reason we have 2 responses, we add one more response
+        let userData = firstResponse[0];//we put a response from JSON to userData variable
+        let repoData = secondResponse[0];//we put a response from JSON to userData variable
+        //when we do two calls like this, the when() method packs a response up into arrays. And each one is the first element of the array. So we just need to put the indexes in there for these responses.
+
         $('#gh-user-data').html(userInformationHTML(userData)); // we select a user data div and set the result to another function called userInformationHTML with argument of userData
+        $('#gh-repo-data').html(repoInformationHTML(repoData));
     }, function(errorResponse) {
         if (errorResponse.status === 404) { //if 404 error will take place show the text below
             $('#gh-user-data').html(`<h2> No user found for user ${username}`)
@@ -56,3 +88,4 @@ $.when(
 }
     //username is value
 //in this case 1st argument is function 
+$(document).ready(fetchGitHubInformation);
